20a573f64386da77f9bcad1b34537d00fa0ffd6c
bnguien <nguyennguyendangbao8@gmail.com>
2025-11-09 11:58:27 +0700

    Update TaskBO.java

--- convert_pdf_to_word/src/main/java/model/bo/TaskBO.java
+++ convert_pdf_to_word/src/main/java/model/bo/TaskBO.java
@@ -1,40 +1,20 @@
 package model.bo;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-
+import java.util.List;
 import model.bean.Task;
 import model.dao.TaskDAO;
-import model.enums.TaskStatus;
-
 public class TaskBO {
-    private final TaskDAO taskDAO = new TaskDAO();
-    
-    public Task processAndSaveTask(int userId, String fileName, String contentType, InputStream contentFile, String baseDir) {
-        File userUploadDir = new File(baseDir + File.separator + userId);
-        if (!userUploadDir.exists()) userUploadDir.mkdirs(); 
+    private TaskDAO taskDAO = new TaskDAO();
 
-        String uniqueFileName = System.currentTimeMillis() + "_" + fileName;
-        Path targetPath = Paths.get(userUploadDir.getAbsolutePath() + File.separator + uniqueFileName);
-
-        try {
-            Files.copy(contentFile, targetPath, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null; 
-        }
-
-        Task task = new Task();
-        task.setUserId(userId);
-        task.setPdfName(fileName);
-        task.setPdfPath(targetPath.toString());
-        task.setStatus(TaskStatus.UPLOADED);
-        boolean success = taskDAO.createTask(task); 
-
-        return success ? task : null;
+    public int addTask(Task task){
+        return taskDAO.insertTask(task);
+    }
+    public List<Task> getUserTasks(int userId){
+        return taskDAO.getTasksByUserId(userId);
+    }
+    public void updateTask(int taskId, String status, String docxPath){
+        taskDAO.updateTaskStatus(taskId, status, docxPath);
+    }
+    public void saveHistory(String username, String originalFileName, String storedFileName){
+        taskDAO.saveConversionHistory(username, originalFileName, storedFileName);
     }
 }
